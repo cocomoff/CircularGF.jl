e2b0aad2e0f5a1a8b38b38f7300d1e490b776a92
Keisuke Otaki <keisuke.otaki.jp@gmail.com>
2024-09-04 10:54:08 +0900

    bugfix, normalization

--- src/geofence.jl
+++ src/geofence.jl
@@ -5,9 +5,16 @@ using CSV
 const WCP_x = 448175.70093339786
 const WCP_y = 4417804.258708443
 
-function computing_geofence(data="./data/data_WCP500.csv"; POIx=WCP_x, POIy=WCP_y, μ=2000, crlimit=0.5)
+function computing_geofence(data="./data/data_WCP500.csv"; POIx=WCP_x, POIy=WCP_y, μ=2000, crlimit=0.5, normalize=false)
     # データをユーザごとに分割する
-    df = CSV.File("./data/data_WCP500.csv") |> DataFrame
+    df = CSV.File(data) |> DataFrame
+    if normalize
+        # (x, y) を[0, 1] に変換する
+        max_x, min_x = maximum(df.x), minimum(df.x)
+        max_y, min_y = maximum(df.y), minimum(df.y)
+        df[:, "x"] = (df.x .- min_x) ./ (max_x - min_x)
+        df[:, "y"] = (df.y .- min_y) ./ (max_y - min_y)
+    end
     unique_users = unique(df.user)
     list_df = [df[df.user .== user, :] for user in unique_users]
 
@@ -43,7 +50,16 @@ function computing_geofence(data="./data/data_WCP500.csv"; POIx=WCP_x, POIy=WCP_
 
     # ジオフェンス計算
     # 何も設定していないECA()でとりあえずfitnessを最適化する
-    bounds = [POIx - 1000 POIx + 1000; POIy - 1000 POIy + 1000; 100 500]
+    if normalize
+        nPOIx = (POIx - min_x) / (max_x - min_x)
+        nPOIy = (POIy - min_y) / (max_y - min_y)
+        @info "POI=($POIx,$POIy)"
+        @info "nPOI=($nPOIx, $nPOIy)"
+        # bounds = [0.0 1.0; 0.0 1.0; 0.0 0.5]
+        bounds = [nPOIx - 0.7 nPOIx + 0.7; nPOIy - 0.7 nPOIy + 0.7; 0.0 0.5]
+    else
+        bounds = [POIx - 1000 POIx + 1000; POIy - 1000 POIy + 1000; 100 500]
+    end
     result = optimize(fitness, bounds, ECA())
     X, Y, R = minimizer(result)
     @info "X=$X, Y=$Y, R=$R"
--- src/visualize.jl
+++ src/visualize.jl
@@ -13,8 +13,17 @@ function circleShape(h, k, r)
     h .+ r * sin.(theta), k .+ r * cos.(theta)
 end
 
-function main(; X=WCP_x, Y=WCP_y)
-    df = CSV.File("./data/data_WCP500.csv") |> DataFrame
+function main(data="./data/data_WCP500.csv"; X=WCP_x, Y=WCP_y, normalize=false)
+    df = CSV.File(data) |> DataFrame
+
+    if normalize
+        # (x, y) を[0, 1] に変換する
+        max_x, min_x = maximum(df.x), minimum(df.x)
+        max_y, min_y = maximum(df.y), minimum(df.y)
+        df[:, "norm_x"] = (df.x .- min_x) ./ (max_x - min_x)
+        df[:, "norm_y"] = (df.y .- min_y) ./ (max_y - min_y)
+    end
+
 
     # unique user list
     unique_users = unique(df.user)
@@ -27,15 +36,34 @@ function main(; X=WCP_x, Y=WCP_y)
     for (i, user) in enumerate(unique_users)
         ci = colors[i]
         df_user = df[df.user.==user, :]
-        scatter!(fig, df_user.x, df_user.y, ms=2, color=ci, alpha=0.25, label=nothing)
+
+        if normalize
+            scatter!(fig, df_user.norm_x, df_user.norm_y, ms=2, color=ci, alpha=0.25, label=nothing)
+        else
+            scatter!(fig, df_user.x, df_user.y, ms=2, color=ci, alpha=0.25, label=nothing)
+        end
     end
-    scatter!(fig, [X], [Y], marker=:x, c=:red, label=nothing)
-    savefig(fig, "figures/data.png")
+    if normalize
+        scatter!(fig, [(X - min_x) / (max_x - min_x)], [(Y - min_y) / (max_y - min_y)], marker=:x, c=:red, label=nothing)
+    else
+        scatter!(fig, [X], [Y], marker=:x, c=:red, label=nothing)
+    end
+    suffix = normalize ? "_norm" : ""
+    savefig(fig, "figures/data$(suffix).png")
 end
 
 
-function main_with_gf(; POIx=WCP_x, POIy=WCP_y)
-    df = CSV.File("./data/data_WCP500.csv") |> DataFrame
+function main_with_gf(data="./data/data_WCP500.csv"; POIx=WCP_x, POIy=WCP_y, normalize=false)
+    df = CSV.File(data) |> DataFrame
+    max_x, min_x = maximum(df.x), minimum(df.x)
+    max_y, min_y = maximum(df.y), minimum(df.y)
+
+    if normalize
+        # (x, y) を[0, 1] に変換する    
+        df[:, "norm_x"] = (df.x .- min_x) ./ (max_x - min_x)
+        df[:, "norm_y"] = (df.y .- min_y) ./ (max_y - min_y)
+    end
+
 
     # unique user list
     unique_users = unique(df.user)
@@ -43,21 +71,88 @@ function main_with_gf(; POIx=WCP_x, POIy=WCP_y)
     # 描画
     fig = plot(size=(500, 500))
 
-    X, Y, R = computing_geofence()
+    X, Y, R = computing_geofence(data; normalize=normalize)
 
     colors = distinguishable_colors(length(unique_users))
     for (i, user) in enumerate(unique_users)
         ci = colors[i]
         df_user = df[df.user.==user, :]
-        scatter!(fig, df_user.x, df_user.y, ms=2, color=ci, alpha=0.25, label=nothing)
+        if normalize
+            # println(df_user.norm_x)
+            scatter!(fig, df_user.norm_x, df_user.norm_y, ms=2, color=ci, alpha=0.25, label=nothing)
+        else
+            scatter!(fig, df_user.x, df_user.y, ms=2, color=ci, alpha=0.25, label=nothing)
+        end
     end
+
     # POI
-    scatter!(fig, [POIx], [POIy], marker=:x, c=:red, label=nothing)
+    if normalize
+        scatter!(fig, [(POIx - min_x) / (max_x - min_x)], [(POIy - min_y) / (max_y - min_y)], marker=:x, c=:red, label=nothing)
+    else
+        scatter!(fig, [POIx], [POIy], marker=:x, c=:red, label=nothing)
+    end
 
     # ジオフェンス
     plot!(fig, [X], [Y], color=:red, marker=:+, markersize=10, label=nothing)
     plot!(fig, circleShape(X, Y, R), color=:black, seriestype=:shape, fillalpha=0.2, label=nothing, aspect_ratio=:equal)
 
     # 保存
-    savefig(fig, "figures/data_with_gf.png")
+    suffix = normalize ? "_norm" : ""
+    savefig(fig, "figures/data_with_gf$(suffix).png")
+end
+
+
+function main_with_gf_after(data="./data/data_WCP500.csv"; POIx=WCP_x, POIy=WCP_y, normalize=false)
+    df = CSV.File(data) |> DataFrame
+    max_x, min_x = maximum(df.x), minimum(df.x)
+    max_y, min_y = maximum(df.y), minimum(df.y)
+    dx = max_x - min_x
+    dy = max_y - min_y
+
+    if normalize
+        # (x, y) を[0, 1] に変換する    
+        df[:, "norm_x"] = (df.x .- min_x) ./ dx
+        df[:, "norm_y"] = (df.y .- min_y) ./ dy
+    end
+
+
+    # unique user list
+    unique_users = unique(df.user)
+
+    # 描画
+    fig = plot(size=(500, 500))
+
+    X, Y, R = computing_geofence(data)
+
+    colors = distinguishable_colors(length(unique_users))
+    for (i, user) in enumerate(unique_users)
+        ci = colors[i]
+        df_user = df[df.user.==user, :]
+        if normalize
+            # println(df_user.norm_x)
+            scatter!(fig, df_user.norm_x, df_user.norm_y, ms=2, color=ci, alpha=0.25, label=nothing)
+        else
+            scatter!(fig, df_user.x, df_user.y, ms=2, color=ci, alpha=0.25, label=nothing)
+        end
+    end
+
+    # POI
+    if normalize
+        scatter!(fig, [(POIx - min_x) / dx], [(POIy - min_y) / dy], marker=:x, c=:red, label=nothing)
+    else
+        scatter!(fig, [POIx], [POIy], marker=:x, c=:red, label=nothing)
+    end
+
+    # normalized ジオフェンス
+    # TODO: fix normalized eq.
+    nX = (X - min_x) / dx
+    nY = (Y - min_y) / dy
+    nR = R / sqrt(dx ^2 + dy ^ 2)
+    @info "(X,Y,R)=($X,$Y,$R)"
+    @info "norm (X,Y,R)=($nX,$nY,$nR)"
+    plot!(fig, [nX], [nY], color=:red, marker=:+, markersize=10, label=nothing)
+    plot!(fig, circleShape(nX, nY, nR), color=:black, seriestype=:shape, fillalpha=0.2, label=nothing, aspect_ratio=:equal)
+
+    # 保存
+    savefig(fig, "figures/data_with_gfa.png")
 end
